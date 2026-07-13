5ebebe4814bf052f8db7820b3e235322845fb34e
OliviaSYS <twinoliviapu@gmail.com>
2026-07-13 08:31:11 -0400

    following guide (4th step)

--- script.js
+++ script.js
@@ -1,3 +1,113 @@
 setInterval(function () {
     document.querySelector("#time").innerHTML = new Date().toLocaleString();
 }, 1000);
+
+var selectedIcon = undefined;
+var biggestIndex = 1;
+
+function closeWindow(element) {
+    element.style.display = "none";
+}
+
+function makeClosable(elementName) {
+    const screen = document.querySelector("#" + elementName);
+    const closeButton = document.querySelector("#" + elementName + "close");
+    closeButton.addEventListener("click", () => closeWindow(screen));
+}
+function initializeWindow(elementName) {
+    var screen = document.querySelector("#" + elementName);
+    addWindowTapHandling(screen);
+    makeClosable(elementName);
+    dragElement(screen);
+}
+
+function dragElement(element) {
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+    if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id + "header").onmousedown = startDragging;
+    } else {
+        element.onmousedown = startDragging;
+    }
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX;
+        initialY = e.clientY;
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+    function stopDragging() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+function selectIcon(element) {
+    element.classList.add("selected");
+    selectedIcon = element;
+}
+function deselectIcon(element) {
+    element.classList.remove("selected");
+    selectedIcon = undefined;
+}
+function handleIconTap(element) {
+    if (element.classList.contains("selected")) {
+        deselectIcon(element);
+        openWindow(document.querySelector("#aboutme"));
+    } else {
+        selectedIcon(element);
+    }
+}
+function addWindowTapHandling(element) {
+    element.addEventListener("mousedown", () =>
+        handleWindowTap(element)
+    )
+};
+function handleWindowTap(element) {
+    biggestIndex++;
+    element.style.zIndex = biggestIndex;
+    topBar.style.zIndex = biggestIndex+1;
+    deselectIcon(selectedIcon);
+}
+function openWindow(element) {
+    element.style.display="flex";
+    biggestIndex++;
+    element.style.zIndex = biggestIndex;
+    topBar.style.zIndex = biggestIndex+1;
+}
+
+var topBar = document.querySelector("#top");
+
+var welcomeScreen = document.querySelector("#welcome");
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+
+initializeWindow("aboutme");
+initializeWindow("welcome");
+var aboutmeScreen = document.querySelector("#aboutme");
+var aboutmeScreenClose = document.querySelector("#aboutmeclose")
+aboutmeScreenClose.addEventListener("click", () => closeWindow(aboutmeScreen));
+
+var welcomeScreen = document.querySelector("#welcome");
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+welcomeScreenClose.addEventListener("click", function() {
+    closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+    openWindow(welcomeScreen);
+});
+addWindowTapHandling(welcomeScreen);
+addWindowTapHandling(aboutmeScreen);
